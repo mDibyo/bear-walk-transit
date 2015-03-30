19bc72660eab07eb778d73b53adedcefbfd5d7dc
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-03-30 15:49:16 -0700

    Minor change to data recorder

--- data_recorder.py
+++ data_recorder.py
@@ -1,7 +1,11 @@
+import os
+os.environ['http_proxy'] = ''
+
 import urllib2
 import urlparse
 import time
 import datetime
+import json
 
 
 class BearwalkDataRecorder(object):
@@ -12,21 +16,31 @@ class BearwalkDataRecorder(object):
                                         params='',
                                         query='',
                                         fragment='').geturl()
+        self.url = urlparse.ParseResult(scheme='http',
+                                        netloc='bearwalk.berkeley.edu',
+                                        path='/bustracking/api/v1/positions',
+                                        params='',
+                                        query='',
+                                        fragment='').geturl()
         self.request = urllib2.Request(self.url, '')
         self.file = file
 
     def get_response(self):
-        print 'requesting'
-        response = urllib2.urlopen(self.request)
-        return json.loads(response.read())
+        try:
+            print 'requesting'
+            response = urllib2.urlopen(self.url)
+            return json.loads(response.read())
+        except (urllib2.URLError, urllib2.HTTPError) as e:
+            print 'request failed: {}'.format(e)
+        except Exception as e:
+            print e
 
     def record(self):
         with open(self.file, 'a') as f:
             while True:
                 data = self.get_response()
                 string = json.dumps({str(datetime.datetime.now()): data})
-                f.write('{}\n'.format(string))
+                f.write('{}\n$$$\n'.format(string))
 
                 time.sleep(5)
-
 
